f628821a116f58944d98be5bea0f6deefdf61dea
sunndayy <cuduong30121997@gmail.com>
2019-04-08 16:26:35 +0700

    add filter + create

--- HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -33,182 +33,190 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        Observable.fromArray(1,2,3,4,5).subscribe(new Observer<Integer>() {
+
+        // ---------------- Creating Observables
+
+//        Observable.create(new ObservableOnSubscribe<Integer>() {
+//            @Override
+//            public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+//                try {
+//                    for (int i = 1; i < 5; i++) {
+//                        emitter.onNext(i);
+//                    }
+//                    emitter.onComplete();
+//                }
+//                catch (Exception e)
+//                {
+//                    emitter.onError(e);
+//                }
+//            }
+//
+//
+//        } ).subscribe(new Observer<Integer>() {
+//
+//
 //            @Override
 //            public void onSubscribe(Disposable d) {
 //
 //            }
 //
 //            @Override
-//            public void onNext(Integer integer) {
-//                Log.d("from", String.valueOf(integer));
+//            public void onNext(Integer item) {
+//                Log.d("onNext", String.valueOf(item));
 //            }
 //
 //            @Override
 //            public void onError(Throwable e) {
-//
+//                Log.d("onError", e.getMessage());
 //            }
 //
 //            @Override
 //            public void onComplete() {
-//                Log.d("from", "Completed");
+//                Log.d("onComplete",  "Sequence complete.");
 //            }
 //        });
 
+        //***********Just*******************//
 
-//        Integer[] arr = {1,2,3,4,5};
-//        Observable.just(arr).subscribe(new Observer<Integer[]>() {
-//            @Override
-//            public void onSubscribe(Disposable d) {
+        //1
+//        String greeting = "Hello world!";
 //
-//            }
+//        Observable<String> observable = Observable.just(greeting);
 //
-//            @Override
-//            public void onNext(Integer[] integers) {
-//                Log.d("just", Arrays.toString(integers));
-//            }
+//        observable.subscribe(item -> Log.d("onNext",item));
 //
-//            @Override
-//            public void onError(Throwable e) {
 //
-//            }
+        //2
+//        Observable<Object> observable1 = Observable.just("1", "A", "3.2", "def");
 //
-//            @Override
-//            public void onComplete() {
-//                Log.d("just", "Completed");
-//            }
-//        });
+//        observable1.subscribe(item -> Log.d("onNext", String.valueOf(item)), error -> error.printStackTrace(),
+//                () -> System.out.println());
 
-//        Observable.interval(2, TimeUnit.SECONDS).subscribe(new Observer<Long>() {
-//            @Override
-//            public void onSubscribe(Disposable d) {
+        //************from**************//
+
+//        List<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8));
 //
-//            }
+//        Observable<Integer> observable = Observable.fromIterable(list);
 //
-//            @Override
-//            public void onNext(Long aLong) {
-//                Log.d("interval", String.valueOf(aLong));
-//            }
-//
-//            @Override
-//            public void onError(Throwable e) {
-//
-//            }
-//
-//            @Override
-//            public void onComplete() {
-//                Log.d(" interval", "Completed");
-//            }
-//        });
+//        observable.subscribe(item -> Log.d("onNext", String.valueOf(item)), error -> error.printStackTrace(),
+//                () -> System.out.println("Done"));
+
+        //**********defer***********//
 
 //        Observable<Long> observable = Observable.defer(() -> {
-//           long time = System.currentTimeMillis();
-//           return Observable.just(time);
+//            long time = System.currentTimeMillis();
+//            return Observable.just(time);
 //        });
 //
-//        observable.subscribe(time -> Log.d("defer", String.valueOf(time)));
+//        observable.subscribe(time -> Log.d("onNext", String.valueOf(time)));
 //
 //        try {
-//            sleep(1000);
+//            Thread.sleep(2000);
 //        } catch (InterruptedException e) {
 //            e.printStackTrace();
 //        }
 //
-//        observable.subscribe(time -> Log.d("defer", String.valueOf(time)));
+//        observable.subscribe(time -> Log.d("onNext", String.valueOf(time)));
 
-//        ObservableOnSubscribe<String> handler = emitter -> {
-//          emitter.onNext("hello");
-//          emitter.onNext("good by");
-//          emitter.onComplete();
-//        };
+        //***********interval***************//
+
+//        Observable<Long> clock = Observable.interval(1, TimeUnit.SECONDS);
 //
-//        Observable.create(handler).subscribe(item -> Log.d("create", item), error -> error.printStackTrace() ,() -> Log.d("create", "completed"));
+//        clock.subscribe(time -> {
+//            if (time % 2 == 0) {
+//                Log.d("onNext", "Tick");
+//            } else {
+//                Log.d("onNext", "Tock");
+//            }
+//        });
 
-//        Observable.just(1,2,3)
-//                .map(x -> x * x)
-//                .subscribe(item -> Log.d("map", String.valueOf(item)));
 
+
+        // ----------------- Filtering Observables
+//        Observable<Integer> observable = Observable.just(1,2,3,4,5,6)
+//                .filter(x -> x % 2 == 0);
+//        observable.subscribe(x-> Log.d("onNext", String.valueOf(x)));
+
+        //****************take*****************//
+
+//        Observable<Integer> observable = Observable.just(1,2,3,4,5,6)
+//                .take(3);
+//        observable.subscribe(x-> Log.d("onNext", String.valueOf(x)));
+
+        //************distinct*******************//
+//
+//        Observable.just(2, 3, 4, 4, 2, 1)
+//                .distinct()
+//                .subscribe(x-> Log.d("onNext", String.valueOf(x)));
+
+        //************skip**************//
+
+//        Observable<Integer> source = Observable.just(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+//
+//        source.skip(4)
+//                .subscribe(x-> Log.d("onNext", String.valueOf(x)));
+
+
+
+        // ----------------- Transforming Observables
 //        Observable.just(1,2,3,4,5,6)
-//                .filter(x -> x%2 == 0)
-//                .subscribe(item -> Log.d("filter", String.valueOf(item)));
+//                .map(x -> x*2)
+//                .subscribe(item -> Log.d("map", String.valueOf(item)), err -> {}, () -> {});
+//
+//        Observable.just(1,2,3)
+//                .flatMap(x -> {
+//                    return Observable.just(x +"a", x + "b" , x + "c");
+//                }).subscribe(item -> Log.d("flatmap", item), errr -> {}, () -> {});
 
+
+        // ----------------- Combining Observables
 //        Observable.just(1,2,3)
 //                .mergeWith(Observable.just(4,5,6))
-//                .subscribe(item -> Log.d("merge", String.valueOf(item)));
+//                .subscribe(item -> Log.d("merge", String.valueOf(item)), err -> {}, () -> {});
+//
+//        Observable.just("Nguyen", "Tran", "Le")
+//                .zipWith(Observable.just("Duc", "Hai", "Dung"), (a, b) -> a + " " + b)
+//                .subscribe(item -> Log.d("zip", item), err -> {}, () -> {});
 
-//        Observable<String> firstNames = Observable.just("James", "Jean-Luc", "Benjamin");
-//        Observable<String> lastNames = Observable.just("Kirk", "Picard", "Sisko");
-//        firstNames.zipWith(lastNames, (first, last) -> first + " " + last)
-//                .subscribe(item -> Log.d("zip", item));
+        // ------------------ Observable Utility Operators
 
-//        Disposable disposable = Observable.just(1,2,3).subscribe();
-//        disposable.dispose();
-
-        // Creating Observables
-
-
-        // Filtering Observables
-
-
-        // Transforming Observables
-        Observable.just(1,2,3,4,5,6)
-                .map(x -> x*2)
-                .subscribe(item -> Log.d("map", String.valueOf(item)), err -> {}, () -> {});
-
-        Observable.just(1,2,3)
-                .flatMap(x -> {
-                    return Observable.just(x +"a", x + "b" , x + "c");
-                }).subscribe(item -> Log.d("flatmap", item), errr -> {}, () -> {});
-
-
-        // Combining Observables
-        Observable.just(1,2,3)
-                .mergeWith(Observable.just(4,5,6))
-                .subscribe(item -> Log.d("merge", String.valueOf(item)), err -> {}, () -> {});
-
-        Observable.just("Nguyen", "Tran", "Le")
-                .zipWith(Observable.just("Duc", "Hai", "Dung"), (a, b) -> a + " " + b)
-                .subscribe(item -> Log.d("zip", item), err -> {}, () -> {});
-
-        // Observable Utility Operators
-
-        Integer[][] array = new Integer[][]{{1,2,3,4,5,6},{11,22,33,44,55},{111,222,333,444,555}};
-        Observable<Integer[]> observable = Observable.fromArray(array);
-        Observable delay=observable.delay(10,TimeUnit.SECONDS);
-        delay.flatMap(new Function<Integer[], ObservableSource<?>>() {
-
-            @Override
-            public ObservableSource<?> apply(Integer[] integers) throws Exception {
-                Log.d("tag","FlatMap"+Thread.currentThread().getName());
-                return Observable.fromArray(integers);
-            }
-        }).filter(new Predicate() {
-            @Override
-            public boolean test(Object o) throws Exception {
-                return Integer.parseInt(o.toString())%2==1;
-            }
-        }).observeOn(Schedulers.newThread())
-                .subscribeOn(Schedulers.newThread())
-                .subscribe(new Observer<Object>() {
-                    @Override
-                    public void onSubscribe(Disposable d) {
-
-                    }
-
-                    @Override
-                    public void onNext(Object o) {
-                        Log.d("tag","Subscribe"+o.toString()+" "+Thread.currentThread().getName());
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-
-                    }
-
-                    @Override
-                    public void onComplete() {
-
-                    }
-                });
+//        Integer[][] array = new Integer[][]{{1,2,3,4,5,6},{11,22,33,44,55},{111,222,333,444,555}};
+//        Observable<Integer[]> observable = Observable.fromArray(array);
+//        Observable delay=observable.delay(10,TimeUnit.SECONDS);
+//        delay.flatMap(new Function<Integer[], ObservableSource<?>>() {
+//
+//            @Override
+//            public ObservableSource<?> apply(Integer[] integers) throws Exception {
+//                Log.d("tag","FlatMap"+Thread.currentThread().getName());
+//                return Observable.fromArray(integers);
+//            }
+//        }).filter(new Predicate() {
+//            @Override
+//            public boolean test(Object o) throws Exception {
+//                return Integer.parseInt(o.toString())%2==1;
+//            }
+//        }).observeOn(Schedulers.newThread())
+//                .subscribeOn(Schedulers.newThread())
+//                .subscribe(new Observer<Object>() {
+//                    @Override
+//                    public void onSubscribe(Disposable d) {
+//
+//                    }
+//
+//                    @Override
+//                    public void onNext(Object o) {
+//                        Log.d("tag","Subscribe"+o.toString()+" "+Thread.currentThread().getName());
+//                    }
+//
+//                    @Override
+//                    public void onError(Throwable e) {
+//
+//                    }
+//
+//                    @Override
+//                    public void onComplete() {
+//
+//                    }
+//                });
     }
 }
