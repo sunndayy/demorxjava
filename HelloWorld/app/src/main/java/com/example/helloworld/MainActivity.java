fe6c9f6bfc8428edf86f42540ccf85ee9c79e0f6
sunndayy <cuduong30121997@gmail.com>
2019-04-08 17:12:39 +0700

    chang source

--- HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -13,6 +13,7 @@ import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.ObservableSource;
 import io.reactivex.Observer;
@@ -33,9 +34,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        // ---------------- Creating Observables
-
+        // Creating Observables
 //        Observable.create(new ObservableOnSubscribe<Integer>() {
 //            @Override
 //            public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
@@ -89,17 +88,20 @@ public class MainActivity extends AppCompatActivity {
         //2
 //        Observable<Object> observable1 = Observable.just("1", "A", "3.2", "def");
 //
-//        observable1.subscribe(item -> Log.d("onNext", String.valueOf(item)), error -> error.printStackTrace(),
-//                () -> System.out.println());
+//        observable1.subscribe(item -> Log.d("onNext", String.valueOf(item)));
 
         //************from**************//
 
-//        List<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8));
+
+//        Integer[] array = new Integer[10];
+//        for (int i = 0; i < array.length; i++) {
+//            array[i] = i;
+//        }
 //
-//        Observable<Integer> observable = Observable.fromIterable(list);
+//        Observable<Integer> observable = Observable.fromArray(array);
 //
-//        observable.subscribe(item -> Log.d("onNext", String.valueOf(item)), error -> error.printStackTrace(),
-//                () -> System.out.println("Done"));
+//        observable.subscribe(item -> Log.d("onNext", String.valueOf(item)));
+
 
         //**********defer***********//
 
@@ -131,8 +133,7 @@ public class MainActivity extends AppCompatActivity {
 //        });
 
 
-
-        // ----------------- Filtering Observables
+        // Filtering Observables
 //        Observable<Integer> observable = Observable.just(1,2,3,4,5,6)
 //                .filter(x -> x % 2 == 0);
 //        observable.subscribe(x-> Log.d("onNext", String.valueOf(x)));
@@ -157,8 +158,7 @@ public class MainActivity extends AppCompatActivity {
 //                .subscribe(x-> Log.d("onNext", String.valueOf(x)));
 
 
-
-        // ----------------- Transforming Observables
+        // Transforming Observables
 //        Observable.just(1,2,3,4,5,6)
 //                .map(x -> x*2)
 //                .subscribe(item -> Log.d("map", String.valueOf(item)), err -> {}, () -> {});
@@ -167,9 +167,9 @@ public class MainActivity extends AppCompatActivity {
 //                .flatMap(x -> {
 //                    return Observable.just(x +"a", x + "b" , x + "c");
 //                }).subscribe(item -> Log.d("flatmap", item), errr -> {}, () -> {});
-
-
-        // ----------------- Combining Observables
+//
+//
+//        // Combining Observables
 //        Observable.just(1,2,3)
 //                .mergeWith(Observable.just(4,5,6))
 //                .subscribe(item -> Log.d("merge", String.valueOf(item)), err -> {}, () -> {});
@@ -177,9 +177,9 @@ public class MainActivity extends AppCompatActivity {
 //        Observable.just("Nguyen", "Tran", "Le")
 //                .zipWith(Observable.just("Duc", "Hai", "Dung"), (a, b) -> a + " " + b)
 //                .subscribe(item -> Log.d("zip", item), err -> {}, () -> {});
-
-        // ------------------ Observable Utility Operators
-
+//
+//        // Observable Utility Operators
+//
 //        Integer[][] array = new Integer[][]{{1,2,3,4,5,6},{11,22,33,44,55},{111,222,333,444,555}};
 //        Observable<Integer[]> observable = Observable.fromArray(array);
 //        Observable delay=observable.delay(10,TimeUnit.SECONDS);
